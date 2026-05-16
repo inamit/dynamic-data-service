e480437c6d6c03e70b120165dcac8e719c9e9ea7
Amit Inbar <inamit323@gmail.com>
2026-05-16 16:34:44 +0300

    Initial commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/amitinbar/dynamicdataservice/DynamicDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.amitinbar.dynamicdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicDataServiceApplication.class, args);
+    }
+
+}
